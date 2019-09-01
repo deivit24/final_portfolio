fd693398105f6426d47fdf76c36cfff7d5a75d94
David Salazar <david.asal@hotmail.com>
2019-09-01 19:21:29 -0400

    test 4

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,27 @@
-// // appear left to right
+// appear left to right
 
-// // BLURB STUFF
-// var items = new ScrollMagic.Controller();
+// BLURB STUFF
+var items = new ScrollMagic.Controller();
 
-// $('.item-left').each(function() {
-//   var itemSlide = new ScrollMagic.Scene({
-//     triggerElement: this,
-//     triggerHook: 0.7,
-//     duration: '80%'
-//   })
-//     .setClassToggle(this, 'items-appear')
-//     .addIndicators()
-//     .addTo(items);
-// });
+$('.item-left').each(function() {
+  var itemSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 1
+  })
+    .setClassToggle(this, 'items-appear')
+    .addIndicators()
+    .addTo(items);
+});
 
-// $('.item-right').each(function() {
-//   var itemSlide = new ScrollMagic.Scene({
-//     triggerElement: this,
-//     triggerHook: 0.7,
-//     duration: '80%'
-//   })
-//     .setClassToggle(this, 'items-appear')
-//     .addIndicators()
-//     .addTo(items);
-// });
+$('.item-right').each(function() {
+  var itemSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 1
+  })
+    .setClassToggle(this, 'items-appear')
+    .addIndicators()
+    .addTo(items);
+});
 
 // Full Page JS
 
