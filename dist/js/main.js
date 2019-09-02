5ce59aea8335e3804b727f8a91dd9c9aecbfdf8f
David Salazar <david.asal@hotmail.com>
2019-09-02 18:04:46 -0400

    test 9 animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,29 @@
-// // appear left to right
+// appear left to right
 
-// // BLURB STUFF
-// var items = new ScrollMagic.Controller();
+// BLURB STUFF
+var items = new ScrollMagic.Controller();
 
-// $('.item-left').each(function() {
-//   var itemSlide = new ScrollMagic.Scene({
-//     triggerElement: this,
-//     triggerHook: 1,
-//     duration: '99%'
-//   })
-//     .setClassToggle(this, 'items-appear')
-//     .addIndicators()
-//     .addTo(items);
-// });
+$('.item-left').each(function() {
+  var itemSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 1,
+    duration: '99%'
+  })
+    .setClassToggle(this, 'items-appear')
+    .addIndicators()
+    .addTo(items);
+});
 
-// $('.item-right').each(function() {
-//   var itemSlide = new ScrollMagic.Scene({
-//     triggerElement: this,
-//     triggerHook: 1,
-//     duration: '99%'
-//   })
-//     .setClassToggle(this, 'items-appear')
-//     .addIndicators()
-//     .addTo(items);
-// });
+$('.item-right').each(function() {
+  var itemSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 1,
+    duration: '99%'
+  })
+    .setClassToggle(this, 'items-appear')
+    .addIndicators()
+    .addTo(items);
+});
 
 // Full Page JS
 
@@ -31,6 +31,5 @@ new fullpage('#main', {
   autoScrolling: true,
   anchors: ['HomePage', 'BioPage', 'KnowledgePage', 'ContactPage', 'WorkPage'],
   navigation: true,
-  navigationPosition: 'left',
-  navigationTooltips: ['Home', 'Bio', 'Knowledge', 'Contact', 'Work']
+  navigationPosition: 'left'
 });
