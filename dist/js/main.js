b845c48ff2c8a686898605ea8fd2ee66be8de799
David Salazar <david.asal@hotmail.com>
2019-09-02 22:29:41 -0400

    added about me and work

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,47 @@
+// Horizontal Slides
+
 // appear left to right
 
-// BLURB STUFF
 var items = new ScrollMagic.Controller();
+var img = new ScrollMagic.Controller();
 
 $('.item-left').each(function() {
   var itemSlide = new ScrollMagic.Scene({
     triggerElement: this,
-    triggerHook: 0.7,
+    triggerHook: 0.9,
     duration: '99%'
   })
     .setClassToggle(this, 'items-appear')
-    .addIndicators()
+    // .addIndicators()
     .addTo(items);
 });
 
 $('.item-right').each(function() {
   var itemSlide = new ScrollMagic.Scene({
     triggerElement: this,
-    triggerHook: 0.7,
+    triggerHook: 0.9,
     duration: '99%'
   })
     .setClassToggle(this, 'items-appear')
-    .addIndicators()
+    // .addIndicators()
     .addTo(items);
+});
+$('.img-cover').each(function() {
+  var imgSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.7,
+    duration: '99%'
+  })
+    .setClassToggle(this, 'img-appear')
+    // .addIndicators()
+    .addTo(img);
 });
 
 // Full Page JS
 
 new fullpage('#main', {
   autoScrolling: true,
-  anchors: ['HomePage', 'BioPage', 'KnowledgePage', 'ContactPage', 'WorkPage'],
+  anchors: ['Home', 'Bio', 'Work', 'Knowledge', 'Contact'],
   navigation: true,
   navigationPosition: 'left'
 });
