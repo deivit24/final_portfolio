29dcb431208486c13147172af38dbe26da8268e1
David Salazar <david.asal@hotmail.com>
2019-09-12 10:44:03 -0500

    added firt project

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,38 +1,35 @@
 // Horizontal Slides
 // work page sliding
 
-$(function() {
-  // wait for document ready
-  // init
-  var wipeani = new ScrollMagic.Controller();
+// wait for document ready
+// init
+var wipeani = new ScrollMagic.Controller();
 
-  // define movement of panels
-  var wipeAnimation = new TimelineMax()
-    // animate to second panel
-    .to('#slideContainer', 0.5, { z: -150 }) // move back in 3D space
-    .to('#slideContainer', 1, { x: '-25%' }) // move in to first panel
-    .to('#slideContainer', 0.5, { z: 0 }) // move back to origin in 3D space
-    // animate to third panel
-    .to('#slideContainer', 0.5, { z: -150, delay: 1 })
-    .to('#slideContainer', 1, { x: '-50%' })
-    .to('#slideContainer', 0.5, { z: 0 })
-    // animate to forth panel
-    .to('#slideContainer', 0.5, { z: -150, delay: 1 })
-    .to('#slideContainer', 1, { x: '-75%' })
-    .to('#slideContainer', 0.5, { z: 0 });
+// define movement of panels
+var wipeAnimation = new TimelineMax()
+  // animate to second panel
+  .to('#slideContainer', 0.5, { z: -150 }) // move back in 3D space
+  .to('#slideContainer', 1, { x: '-25%' }) // move in to first panel
+  .to('#slideContainer', 0.5, { z: 0 }) // move back to origin in 3D space
+  // animate to third panel
+  .to('#slideContainer', 0.5, { z: -150, delay: 1 })
+  .to('#slideContainer', 1, { x: '-50%' })
+  .to('#slideContainer', 0.5, { z: 0 })
+  // animate to forth panel
+  .to('#slideContainer', 0.5, { z: -150, delay: 1 })
+  .to('#slideContainer', 1, { x: '-75%' })
+  .to('#slideContainer', 0.5, { z: 0 });
 
-  // create scene to pin and link animation
-  new ScrollMagic.Scene({
-    triggerElement: '#scrolling-work',
-    triggerHook: 'onLeave',
-    duration: '500%'
-  })
-    .setPin('#scrolling-work')
-    .setTween(wipeAnimation)
-    // add indicators (requires plugin)
-    .addTo(wipeani);
-});
-
+// create scene to pin and link animation
+new ScrollMagic.Scene({
+  triggerElement: '#scrolling-work',
+  triggerHook: 'onLeave',
+  duration: '500%'
+})
+  .setPin('#scrolling-work')
+  .setTween(wipeAnimation)
+  // add indicators (requires plugin)
+  .addTo(wipeani);
 // appear left to right
 
 var items = new ScrollMagic.Controller();
@@ -43,7 +40,14 @@ var scroll = new ScrollMagic.Controller();
 
 new ScrollMagic.Scene({ triggerElement: '.img-bio' })
   .setClassToggle('.img-bio', 'img-bio-about') // add class toggle
-  // .addIndicators() // add indicators (requires plugin)
+  // add indicators (requires plugin)
+  .addTo(sectionAppear);
+
+new ScrollMagic.Scene({
+  triggerElement: '.img-work'
+})
+  .setClassToggle('.img-work', 'img-work-about') // add class toggle
+  // add indicators (requires plugin)
   .addTo(sectionAppear);
 
 $('.item-left').each(function() {
@@ -53,7 +57,7 @@ $('.item-left').each(function() {
     duration: '99%'
   })
     .setClassToggle(this, 'items-appear')
-    // .addIndicators()
+    //
     .addTo(items);
 });
 
@@ -64,7 +68,7 @@ $('.item-right').each(function() {
     duration: '99%'
   })
     .setClassToggle(this, 'items-appear')
-    // .addIndicators()
+    //
     .addTo(items);
 });
 $('.item-up').each(function() {
@@ -74,7 +78,7 @@ $('.item-up').each(function() {
     duration: '99%'
   })
     .setClassToggle(this, 'items-appear')
-    // .addIndicators()
+    //
     .addTo(items);
 });
 $('.item-down').each(function() {
@@ -84,7 +88,7 @@ $('.item-down').each(function() {
     duration: '100%'
   })
     .setClassToggle(this, 'scroll-appear')
-    // .addIndicators()
+    //
     .addTo(items);
 });
 $('.img-cover').each(function() {
@@ -94,7 +98,7 @@ $('.img-cover').each(function() {
     duration: '99%'
   })
     .setClassToggle(this, 'img-appear')
-    // .addIndicators()
+
     .addTo(img);
 });
 
